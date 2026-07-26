2406241ff7f78f9a7c3ec4b86064442d337df67f
headshot2017 <>
2026-07-26 02:28:54 -0400

    set player FaceAngle to direction of travel
    makes the camera face the direction the car is moving
    might change this to the car's front facing angle to be accurate to the original game

--- source/player.cpp
+++ source/player.cpp
@@ -1,3 +1,5 @@
+#include <math.h>
+
 #include <nds.h>
 #include <nds/arm9/videoGL.h>
 
@@ -80,11 +82,19 @@ void CPlayer::Update()
 		b3Body_SetLinearVelocity(m_BodyId, b3Vec3{-8, 0, 0});
 
 	b3Vec3 position = b3Body_GetPosition(m_BodyId);
+	b3Vec3 velocity = b3Body_GetLinearVelocity(m_BodyId);
+	float length = b3Length(velocity);
+	b3Vec3 normalized = b3Normalize(velocity);
 
-	//printf("%.4f %.4f %.4f\n", position.x, position.y, position.z);
 	m_X = floattof32(position.x / SCALE_VERTICES);
 	m_Y = floattof32(position.y / SCALE_VERTICES);
 	m_Z = floattof32(position.z / SCALE_VERTICES);
+
+	if (length >= 0.5f)
+	{
+		float ang = -atan2f(normalized.z, normalized.x);
+		m_FaceAngle = (int)(ang * (DEGREES_IN_CIRCLE>>1) / B3_PI) - (DEGREES_IN_CIRCLE>>2);
+	}
 }
 
 void CPlayer::Render()
@@ -98,14 +108,10 @@ void CPlayer::Render()
 		glRotateYi(m_FaceAngle);
 	else
 	{
-		b3Quat rotation = b3Body_GetRotation(m_BodyId);
-
 		float angle;
-		b3Vec3 axis = b3GetAxisAngle(&angle, rotation);
+		b3Vec3 axis = b3GetAxisAngle(&angle, b3Body_GetRotation(m_BodyId));
 		angle = (angle * B3_RAD_TO_DEG);
 
-		//printf("%.4f %.4f %.4f\n", axis.x * angle, axis.y * angle, axis.z * angle);
-		//m_FaceAngle = floattof32(axis.y * angle) << 3;
 		glRotatef(angle, axis.x, axis.y, axis.z);
 	}
 
